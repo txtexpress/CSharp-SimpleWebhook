bb9282aa64a70c3c7c3337299baedc23924b6087
txt-express <jzapata@txt-express.com>
2019-06-27 13:06:18 -0500

    Simple Webhok CSharp

---
+++ SimpleWebHook/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleWebHook.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+     
+
+        [HttpPost]
+        public JsonResult EchoWebhook(ModelMessage inMsg)
+        { 
+            ModelMessage outMsg = new ModelMessage();
+            outMsg.from = inMsg.from;
+            outMsg.msg = "Echo: " + inMsg.msg;
+            return Json(outMsg, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+    public class ModelMessage
+    {
+        public string from { get; set; }
+        public string msg { get; set; }
+    }
+
+}
---
+++ SimpleWebHook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleWebHook.MvcApplication" Language="C#" %>
